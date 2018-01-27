684b58f24a501a9c16d7b6fe7cb8dded87bde45f
jeffreyh <jeffreyhyw924@gmail.com>
2018-01-27 12:38:00 +0800

    init

---
+++ SmartSociety/SmartSociety/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartSociety
+//
+//  Created by Ho Ying Wai Jeffrey on 27/1/2018.
+//  Copyright © 2018 Ho Ying Wai Jeffrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
